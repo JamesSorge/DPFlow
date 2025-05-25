1574458c78e4303b90536c3540fa97752565b049
James Sorge <drop3b@gmail.com>
2025-05-25 00:42:32 +0000

    Began shell script to download Kubric NK dataset. Began parameter file for reading in/training cross-gated-unit (CGU) block. Adding files for mixture of laplacian loss and metrics. Updated .gitignore to ignore datasets directory. Updated CGU implementation, but need to clarify input tensor(s) prior to concatenation.

--- architecture_files/cross_gated_unit.py
+++ architecture_files/cross_gated_unit.py
@@ -2,25 +2,72 @@ import torch
 import torch.nn as nn
 import torchvision
 
+class DepthWiseConvolution(nn.Module):
+    def __init__(self, num_channel_in, num_channel_out, kernel_dim, stride_size, padding_dim):
+        """
+        @param num_channel_in: 
+        @param num_channel_out:
+        @param kernel_dim: tuple, e.g. (3, 3)
+        @param stride_size: integer, e.g. 1
+        @param padding_dim: integer, e.g. 1
+        """
+        super(DepthWiseConvolution, self).__init__()
+        self.dw_conv = nn.Conv2d(in_channels=num_channel_in, out_channels=num_channel_out, kernel_size=kernel_dim, stride=stride_size, padding=padding_dim, groups=num_channel_in, bias=True)
+
+    def forward(self, input_x):
+        """
+        @param input_x: pytorch data tensor
+        @returns: depth-wise convolution of input data
+        """
+        return self.dw_conv(input_x)
+
 class CrossGatedUnit(nn.Module):
-    def __init__(self, is_self_gate=False):
+    def __init__(self, is_self_gate=False,
+                 number_in_channels=3,
+                 number_out_channels=128,
+                 kernel_dim=(3, 3), 
+                 stride_size=1,
+                 padding_dim=1):
         super(CrossGatedUnit, self).__init__()
         self.is_self_gate = is_self_gate
-        self.conv1_left = nn.Conv2d(in_channels=, out_channels=, kernel_size=(,), stride=1, padding=0)
-        self.convdw_left = nn.Conv2d(in_channels=, out_channels=, kernel_size=(,), stride=1, padding=0)
-        self.conv1_right = nn.Conv2d(in_channels=, out_channels=, kernel_size=(,), stride=1, padding=0)
-        self.convdw_right = nn.Conv2d(in_channels=, out_channels=, kernel_size=(,), stride=1, padding=0)
-        self.final_conv = nn.Conv2d(in_channels=, out_channels=, kernel_size=(,), stride=1, padding=0)
+        self.conv1_left = nn.Conv2d(in_channels=number_in_channels, out_channels=number_out_channels, kernel_size=kernel_dim, stride=stride_size, padding=padding_dim)
+        self.convdw_left = DepthWiseConvolution(num_channel_in=number_out_channels, num_channel_out=number_out_channels, kernel_dim=kernel_dim, stride_size=stride_size, padding_dim=padding_dim)
+        self.conv1_right = nn.Conv2d(in_channels=number_out_channels, out_channels=number_out_channels, kernel_size=kernel_dim, stride=stride_size, padding=padding_dim)
+        self.convdw_right = DepthWiseConvolution(num_channel_in=number_out_channels, num_channel_out=number_out_channels, kernel_dim=kernel_dim, stride_size=stride_size, padding_dim=padding_dim)
+        self.final_conv = nn.Conv2d(in_channels=number_out_channels, out_channels=number_out_channels, kernel_size=kernel_dim, stride=stride_size, padding=padding_dim)
         self.gelu = nn.GELU()
         if is_self_gate: # self-gate
             self.conv1_cross_gate = None
         else: # cross-gate
-            self.conv1_cross_gate = nn.Conv2d(in_channels=, out_channels=, kernel_size=(,), stride=1, padding=0)
+            self.conv1_cross_gate = nn.Conv2d(in_channels=number_out_channels, out_channels=number_out_channels, kernel_size=kernel_dim, stride=stride_size, padding=padding_dim)
 
     def forward(self, input_x):
         """
         @param input_x: Pytorch tensor of dims: [batch size, channel size, height, widith] <--- FIXME: double check for accuracy
+        @returns: 
         """
         if not self.is_self_gate: # cross-gate performs an extra convolution on the two inputs
+            input_x = torch.cat((), dim=)
+            input_x = self.conv1_cross_gate(input_x)
+        else:  # self-gate only
+            # FIXME: Is this the correct way to copy this tensor? Am I detaching from the computation graph too early? See https://stackoverflow.com/questions/55266154/pytorch-preferred-way-to-copy-a-tensor
+            input2 = input_x.clone().detach()
+
+            # Left CGU conv1/convDW
+            output_left = self.conv1_left(input_x)
+            output_left = self.convdw_left(output_left)
+            output_left = self.gelu(output_left)
+
+            # Right CGU conv1/convDW
+            output_right = self.conv1_right(input2)
+            output_right = self.convdw_right(output_right)
+
+            # Elementwise multiplication
+            output = torch.mul(output_left, output_right)
+
+            # Final convolution
+            output = self.final_conv(output)
+
+        return output
             
             
---
+++ get_kubrik_nk_dataset.sh
@@ -0,0 +1,49 @@
+DIR_KUBRIC="datasets/kubric_nk"
+if [ ! -d "$DIR_KUBRIC" ]; then
+    echo "Creating kubric_nk directory"
+    mkdir -p ./datasets/kubric_nk
+    echo "Done."
+else
+    echo "kubric_nk directory exists."
+fi
+
+if [ -z "$(ls -A datasets/kubric_nk)" ]; then
+    # Download files for training/testing
+    echo "Downloading Kubric-NK dataset files."
+    # echo "Please download Kubric-NK dataset from the followwing Google Drive link: https://drive.google.com/drive/folders/1vSShkqyYwLJYX38iJP3kg6x-DMpCn0R6"
+
+    # The following option failed; it appears to work, and downloads an empty zip file.
+    # configs_1k.zip
+    wget --no-check-certificate "https://drive.usercontent.google.com/download?id=136Zl6mTn9CUUMoIS6lBKmVdVmCQg3dFw&export=download&confirm=t&uuid=cd3f32ea-a48d-4d36-860f-7563d5b59e01" -O configs_1k.zip
+    # # configs_2k.zip
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1pkRO3IwjUGwdx6S0ffa8yaX81DKL-lhx&export=download&confirm=t&uuid=cc3d0699-3085-4838-b7d2-545ab7a93a45"
+    # # configs_4k.zip
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1cPPRKHgMY8A1A2KQzdFibxtrkwQmE815&export=download&confirm=t&uuid=8048c3d8-544c-4c3d-aef3-6269fb5507d2"
+    # # configs_8k.zip
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1wno4bNpWReeopNPgrmHeXJwBaGIiJmtz&export=download&confirm=t&uuid=fe7a6eba-90ff-4020-808c-8fae374d6a59"
+
+    # rgba_1k.zip
+    wget --no-check-certificate "https://drive.usercontent.google.com/download?id=16Wz7FQoP3r_l-vAfMgSWPdePG99MqQ_C&export=download&confirm=t&uuid=13297a3b-1894-4b88-b7c4-45ed9ed2e068" -O rgba_1k.zip
+    # # rgba_2k.zip
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1_vXM6AyAswqfyxor5OvbROxx4g-LOpY4&export=download&confirm=t&uuid=74900d06-a029-4d91-a472-51f0040a08bd"
+    # # rgba_4k.zip
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=10wWQ_eT0ObKlf_nrxkYy3deGpMVlzWOC&export=download&confirm=t&uuid=1d09c051-90fb-49fa-a48a-7daeaf4899b7"
+    # # rgba_8k.zip
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1gfOrvOhMAgXoHg1ciyY3dKuO6dbn5imM&export=download&confirm=t&uuid=05777a76-f856-456e-902b-cee3faff0fda"
+
+    # forward_flow_1k.zip
+    wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1x9o8XP4sfdu9XezaVU5bTJW75XJwMj8P&export=download&confirm=t&uuid=840516e0-5b3a-46fe-9635-15fd1dd86422" -O forward_flow_1k.zip
+    # # forward_flow_2k.zip
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=146yJ-NfZZ_p7Off631heKvVh8dckDXZV&export=download&confirm=t&uuid=19ae331d-9522-4a88-9cd7-cf30ff5baaec"
+    # # forward_flow_4k.zip
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1XPoF1xW94Faaz9xvfBJTdOkhv9M9dUIy&export=download&confirm=t&uuid=62fb4e86-e313-4b51-9c82-a981ffe1d9c9"
+    # # forward_flow_8k.zip
+    # wget --no-check-certificate ""
+
+    # Move all downloaded files to the correct directory
+    mv rgba*.zip datasets/kubric_nk
+    mv config*.zip datasets/kubric_nk
+    mv forward_flow*.zip datasets/kubric_nk
+else
+    echo "Dataset files present."
+fi
