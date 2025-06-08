dc1f59752cf601333f104a18503feabc32252e4c
James Sorge <drop3b@gmail.com>
2025-06-08 01:21:50 +0000

    Adjusted shell scripts to get backward_flow data and organize it.

--- get_kubrik_nk_dataset.sh
+++ get_kubrik_nk_dataset.sh
@@ -13,8 +13,8 @@ if [ -z "$(ls -A datasets/kubric_nk)" ]; then
     # echo "Please download Kubric-NK dataset from the followwing Google Drive link: https://drive.google.com/drive/folders/1vSShkqyYwLJYX38iJP3kg6x-DMpCn0R6"
 
     # The following option failed; it appears to work, and downloads an empty zip file.
-    # configs_1k.zip
-    wget --no-check-certificate "https://drive.usercontent.google.com/download?id=136Zl6mTn9CUUMoIS6lBKmVdVmCQg3dFw&export=download&confirm=t&uuid=cd3f32ea-a48d-4d36-860f-7563d5b59e01" -O configs_1k.zip
+    # # configs_1k.zip
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=136Zl6mTn9CUUMoIS6lBKmVdVmCQg3dFw&export=download&confirm=t&uuid=cd3f32ea-a48d-4d36-860f-7563d5b59e01" -O configs_1k.zip
     # # configs_2k.zip
     # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1pkRO3IwjUGwdx6S0ffa8yaX81DKL-lhx&export=download&confirm=t&uuid=cc3d0699-3085-4838-b7d2-545ab7a93a45"
     # # configs_4k.zip
@@ -22,8 +22,8 @@ if [ -z "$(ls -A datasets/kubric_nk)" ]; then
     # # configs_8k.zip
     # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1wno4bNpWReeopNPgrmHeXJwBaGIiJmtz&export=download&confirm=t&uuid=fe7a6eba-90ff-4020-808c-8fae374d6a59"
 
-    # rgba_1k.zip
-    wget --no-check-certificate "https://drive.usercontent.google.com/download?id=16Wz7FQoP3r_l-vAfMgSWPdePG99MqQ_C&export=download&confirm=t&uuid=13297a3b-1894-4b88-b7c4-45ed9ed2e068" -O rgba_1k.zip
+    # # rgba_1k.zip
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=16Wz7FQoP3r_l-vAfMgSWPdePG99MqQ_C&export=download&confirm=t&uuid=13297a3b-1894-4b88-b7c4-45ed9ed2e068" -O rgba_1k.zip
     # # rgba_2k.zip
     # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1_vXM6AyAswqfyxor5OvbROxx4g-LOpY4&export=download&confirm=t&uuid=74900d06-a029-4d91-a472-51f0040a08bd"
     # # rgba_4k.zip
@@ -31,8 +31,8 @@ if [ -z "$(ls -A datasets/kubric_nk)" ]; then
     # # rgba_8k.zip
     # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1gfOrvOhMAgXoHg1ciyY3dKuO6dbn5imM&export=download&confirm=t&uuid=05777a76-f856-456e-902b-cee3faff0fda"
 
-    # forward_flow_1k.zip
-    wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1x9o8XP4sfdu9XezaVU5bTJW75XJwMj8P&export=download&confirm=t&uuid=840516e0-5b3a-46fe-9635-15fd1dd86422" -O forward_flow_1k.zip
+    # # forward_flow_1k.zip
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=1x9o8XP4sfdu9XezaVU5bTJW75XJwMj8P&export=download&confirm=t&uuid=840516e0-5b3a-46fe-9635-15fd1dd86422" -O forward_flow_1k.zip
     # # forward_flow_2k.zip
     # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=146yJ-NfZZ_p7Off631heKvVh8dckDXZV&export=download&confirm=t&uuid=19ae331d-9522-4a88-9cd7-cf30ff5baaec"
     # # forward_flow_4k.zip
@@ -40,10 +40,16 @@ if [ -z "$(ls -A datasets/kubric_nk)" ]; then
     # # forward_flow_8k.zip
     # wget --no-check-certificate ""
 
+    # Backward flow 1k
+    # wget --no-check-certificate "https://drive.usercontent.google.com/download?id=15LOSIQSO6AvQXTT-K-U_47eHZEnr8sWJ&export=download&authuser=0&confirm=t&uuid=fe948903-e8d7-464a-8895-f99d8e4bcbfc&at=ALoNOgmJh_SYbyOsIc71VOjXJecO:1749344728717" -O backward_flow_1k.zip
+
     # Move all downloaded files to the correct directory
-    mv rgba*.zip datasets/kubric_nk
-    mv config*.zip datasets/kubric_nk
-    mv forward_flow*.zip datasets/kubric_nk
+    # mv rgba*.zip datasets/kubric_nk
+    # mv config*.zip datasets/kubric_nk
+    # mv forward_flow*.zip datasets/kubric_nk
+    # mv backward_flow*.zip datasets/kubric_nk
 else
+    wget --no-check-certificate "https://drive.usercontent.google.com/download?id=15LOSIQSO6AvQXTT-K-U_47eHZEnr8sWJ&export=download&authuser=0&confirm=t&uuid=fe948903-e8d7-464a-8895-f99d8e4bcbfc&at=ALoNOgmJh_SYbyOsIc71VOjXJecO:1749344728717" -O backward_flow_1k.zip
+    mv backward_flow*.zip datasets/kubric_nk
     echo "Dataset files present."
 fi
--- organize_kubric_nk.sh
+++ organize_kubric_nk.sh
@@ -3,6 +3,7 @@
 DIR_KUBRIC_1K="datasets/kubric_nk/1k"
 DIR_CONFIGS="datasets/kubric_nk/configs/1k"
 DIR_FORWARD_FLOW="datasets/kubric_nk/forward_flow/1k"
+DIR_BACKWARD_FLOW="datasets/kubric_nk/backward_flow/1k"
 DIR_RGBA="datasets/kubric_nk/rgba/1k"
 if [ ! -d "$DIR_KUBRIC_1K" ]; then
     echo "Creating kubric_nk/1k directory"
@@ -21,20 +22,24 @@ for i in $(seq 0 $MAX)
 do
     echo "i var: $i"
     if [ "$i" -lt "$THRESHOLD" ]; then
-        mkdir -p "${DIR_KUBRIC_1K}/${DIR_ITERABLE_00}${i}"
-        # mv content from directories in Configs
-        mv ./${DIR_CONFIGS}/${DIR_ITERABLE_00}${i}/*.json "${DIR_KUBRIC_1K}/${DIR_ITERABLE_00}${i}"
-        # mv content from directories in RGBA
-        mv ./${DIR_RGBA}/${DIR_ITERABLE_00}${i}/*.png "${DIR_KUBRIC_1K}/${DIR_ITERABLE_00}${i}"
-        # mv content from directories in Forward Flow
-        mv ./${DIR_FORWARD_FLOW}/${DIR_ITERABLE_00}${i}/*.png "${DIR_KUBRIC_1K}/${DIR_ITERABLE_00}${i}"
+        # mkdir -p "${DIR_KUBRIC_1K}/${DIR_ITERABLE_00}${i}"
+        # # mv content from directories in Configs
+        # mv ./${DIR_CONFIGS}/${DIR_ITERABLE_00}${i}/*.json "${DIR_KUBRIC_1K}/${DIR_ITERABLE_00}${i}"
+        # # mv content from directories in RGBA
+        # mv ./${DIR_RGBA}/${DIR_ITERABLE_00}${i}/*.png "${DIR_KUBRIC_1K}/${DIR_ITERABLE_00}${i}"
+        # # mv content from directories in Forward Flow
+        # mv ./${DIR_FORWARD_FLOW}/${DIR_ITERABLE_00}${i}/*.png "${DIR_KUBRIC_1K}/${DIR_ITERABLE_00}${i}"
+        # # mv content from directories in Backward Flow
+        mv ./${DIR_BACKWARD_FLOW}/${DIR_ITERABLE_00}${i}/*.png "${DIR_KUBRIC_1K}/${DIR_ITERABLE_00}${i}"
     else
-        mkdir -p "${DIR_KUBRIC_1K}/${DIR_ITERABLE_0}${i}"
-        # mv content from directories in Configs
-        mv ./${DIR_CONFIGS}/${DIR_ITERABLE_0}${i}/*.json "${DIR_KUBRIC_1K}/${DIR_ITERABLE_0}${i}"
-        # mv content from directories in RGBA
-        mv ./${DIR_RGBA}/${DIR_ITERABLE_0}${i}/*.png "${DIR_KUBRIC_1K}/${DIR_ITERABLE_0}${i}"
-        # mv content from directories in Forward Flow
-        mv ./${DIR_FORWARD_FLOW}/${DIR_ITERABLE_0}${i}/*.png "${DIR_KUBRIC_1K}/${DIR_ITERABLE_0}${i}"
+        # mkdir -p "${DIR_KUBRIC_1K}/${DIR_ITERABLE_0}${i}"
+        # # mv content from directories in Configs
+        # mv ./${DIR_CONFIGS}/${DIR_ITERABLE_0}${i}/*.json "${DIR_KUBRIC_1K}/${DIR_ITERABLE_0}${i}"
+        # # mv content from directories in RGBA
+        # mv ./${DIR_RGBA}/${DIR_ITERABLE_0}${i}/*.png "${DIR_KUBRIC_1K}/${DIR_ITERABLE_0}${i}"
+        # # mv content from directories in Forward Flow
+        # mv ./${DIR_FORWARD_FLOW}/${DIR_ITERABLE_0}${i}/*.png "${DIR_KUBRIC_1K}/${DIR_ITERABLE_0}${i}"
+        # # mv content from directories in Backward Flow
+        mv ./${DIR_BACKWARD_FLOW}/${DIR_ITERABLE_0}${i}/*.png "${DIR_KUBRIC_1K}/${DIR_ITERABLE_0}${i}"
     fi
 done
